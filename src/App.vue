6349316b019cf5e7febc8609d3ed4b887839b34a
Wesley Jakob <lewesleydj@gmail.com>
2023-09-22 13:59:58 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { reactive } from 'vue';
+
+const state = reactive({
+  filtro: 'soma',
+  num1: '',
+  num2: '',
+  resultado: '',
+})
+
+const sum = () => {
+  const resultadoSoma = state.num1 + state.num2
+  state.resultado = resultadoSoma;
+  return state.resultado
+}
+
+const sub = () => {
+  const resultadoSubtracao = state.num1 - state.num2
+  state.resultado = resultadoSubtracao;
+  return state.resultado
+}
+
+const mult = () => {
+  const resultadoMultiplicacao = state.num1 * state.num2
+  state.resultado = resultadoMultiplicacao;
+  return state.resultado
+}
+
+const div = () => {
+  const resultadoDivisao = state.num1 / state.num2
+  state.resultado = resultadoDivisao
+  return state.resultado
+}
+</script>
+
+<template>
+  <h1>ola</h1>
+</template>
+
+<style scoped>
+
+</style>
